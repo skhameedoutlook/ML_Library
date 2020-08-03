e63b7ccaeed45e94377a6e5e81fb9b724688a6a6
Shaik Hameed <skhameed@outlook.com>
2020-08-03 17:25:20 +0530

    DataSet + Linear Regression Code Completed. Testing Remaining.

--- MLTools/LinearRegressionModel.cpp
+++ MLTools/LinearRegressionModel.cpp
@@ -45,10 +45,18 @@ long double calcCorrelationFromString(vector<string> x, vector<string> y, long d
 	return sum/(x.size()-1);
 }
 
+long double stringToLongDouble(string s) {
+	long double item;
+	stringstream strItem(s);
+	strItem >> item;
+	return item;
+}
+
 class LinearRegressionModel {
 private:
 	vector<string> features;
 	vector<string> labels;
+	vector<long double> predicted;
 	vector<long double> errors;
 	long double x_bar;
 	long double y_bar;
@@ -57,9 +65,13 @@ private:
 	long double correlation;
 	long double mss;
 	long double train_percent;
+	long double slope;
+	long double intercept;
+	long double trainRows;
 public:
 	LinearRegressionModel(DataSet* ds, string feature, string label, map<string, unsigned long long>& indices, long double train) {
 		train_percent = train;
+		trainRows = (train/100)*((*ds).getNumRows());
 		for(unsigned long long i = 0; i < (*ds).getNumRows(); i++) {
 			features.push_back((*(*(*(*(*ds).getRows())[i]).getRow())[indices[feature]]).getValue());
 			labels.push_back((*(*(*(*(*ds).getRows())[i]).getRow())[indices[label]]).getValue());
@@ -71,8 +83,36 @@ public:
 		x_sd = calcSDFromString(features, x_bar);
 		y_sd = calcSDFromString(labels, y_bar);
 		correlation = calcCorrelationFromString(features, labels, x_bar, y_bar, x_sd, y_sd);
+		slope = correlation*(y_sd/x_sd);
+		intercept = y_bar - (slope*x_bar);
 	}
 	void predict() {
-
+		predicted.clear();
+		errors.clear();
+		long double predictedValue;
+		for(unsigned long long i = trainRows; i < features.size(); i++) {
+			predictedValue = (slope*stringToLongDouble(features[i])) + intercept;
+			predicted.push_back(predictedValue);
+			errors.push_back(predictedValue-stringToLongDouble(labels[i]));
+		}
+		mss = calcMSS();
+	}
+	long double calcMSS() {
+		long double sum = 0;
+		for(unsigned long long i = 0; i < errors.size(); i++) {
+			sum += (errors[i]*errors[i]);
+		}
+		return sum/errors.size();
+	}
+	long double getMSS() {
+		predict();
+		return mss;
+	}
+	long double displayPrediction() {
+		predict();
+		cout << "Original\t\t\t\tPredicted" << endl;
+		for(unsigned long long i = trainRows; i < features.size(); i++) {
+			cout << stringToLongDouble(features[i]) << "\t\t\t\t" << predicted[i] << endl;
+		}
 	}
 };
